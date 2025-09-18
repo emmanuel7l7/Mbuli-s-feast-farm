bf217e0625e5e047b6ac819d03c4c0cdb37a3799
emmanuel7l7 <134674148+emmanuel7l7@users.noreply.github.com>
2025-09-18 10:29:48 +0300

    Fix Prisma Schema Inconsistency

--- prisma/migrations/20250916080302_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Product" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "price" REAL NOT NULL,
-    "stock" INTEGER NOT NULL,
-    "status" TEXT NOT NULL
-);
